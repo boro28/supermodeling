42e3a0936ccbe5531f9c06acc4390aff1dfebac2
boro <borkowskiszymon28@gmail.com>
2021-01-17 22:06:37 +0100

    Improvments

--- data_assimilation.py
+++ data_assimilation.py
@@ -3,20 +3,31 @@ from adao import adaoBuilder
 from operator import itemgetter
 
 
-def assimilate(initial, yobs, observation_operator, evolution_func, error_vector=None, verbose=False,
-               maximum_number_of_steps=100):
-    xb = list(itemgetter('x', 'y', 'z', 'rho', 'sigma', 'beta')(initial))
+def lorenz_obs_operator(state):
+    x, y, z, rho, sigma, beta = state
+    return [x, y, z]
+
+
+def assimilateLorenz(initial, observations, lorenz_step, error_vector=None, verbose=False,
+                     maximum_number_of_steps=100):
+    def lorenz_evol_step(state):
+        list(np.ravel(state))
+        x, y, z, rho, sigma, beta = state  # Unpack the state vector
+        parameters = list(map(lambda param: abs(param), [rho, sigma, beta]))
+        return np.array([lorenz_step([x, y, z], parameters), parameters]).reshape(-1, 1)
+
+    background = list(itemgetter('x', 'y', 'z', 'rho', 'sigma', 'beta')(initial))
     case = adaoBuilder.New('')
-    case.setBackground(Vector=xb, Stored=True)
+    case.setBackground(Vector=background, Stored=True)
     if error_vector:
         case.setBackgroundError(DiagonalSparseMatrix=error_vector)
     else:
         case.setBackgroundError(ScalarSparseMatrix=1.e4)
     case.setEvolutionError(ScalarSparseMatrix=0.1)
-    case.setEvolutionModel(OneFunction=evolution_func)
-    case.setObservation(VectorSerie=yobs, Stored=True)
+    case.setEvolutionModel(OneFunction=lorenz_evol_step)
+    case.setObservation(VectorSerie=observations, Stored=True)
     case.setObservationError(ScalarSparseMatrix=0.1)
-    case.setObservationOperator(OneFunction=observation_operator)
+    case.setObservationOperator(OneFunction=lorenz_obs_operator)
     case.setAlgorithmParameters(
         Algorithm='4DVAR',
         Parameters={
